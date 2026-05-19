900f76afa0e3b795a1eac362af9066cde9e14838
Manthan <162015711+manthu01@users.noreply.github.com>
2026-05-20 01:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (58.81%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,51 +1,18 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> n1;
-        map<int,int> n2;
+        int i = 0, j = 0;
 
-        int n = nums1.size();
-        int m = nums2.size();
-        
-        int limit = min(n, m);
-
-        int i = 0;
-
-        for(i = 0; i < limit; i++)
+        while(i < nums1.size() && j < nums2.size())
         {
-            n1[nums1[i]]++;
-            n2[nums2[i]]++;
-
-            if(nums1[i] < nums2[i])
-            {
-                if(n1[nums1[i]] >= 1 && n2[nums1[i]] >= 1)
-                    return nums1[i];
-            }
-            else
-            {
-                if(n2[nums2[i]] >= 1 && n1[nums2[i]] >= 1)
-                    return nums2[i];
-            }
-        }
-
-        while(i < n)
-        {
-            n1[nums1[i]]++;
-
-            if(n2[nums1[i]] >= 1)
+            if(nums1[i] == nums2[j])
                 return nums1[i];
 
-            i++;
-        }
+            else if(nums1[i] < nums2[j])
+                i++;
 
-        while(limit < m)
-        {
-            n2[nums2[limit]]++;
-
-            if(n1[nums2[limit]] >= 1)
-                return nums2[limit];
-
-            limit++;
+            else
+                j++;
         }
 
         return -1;
