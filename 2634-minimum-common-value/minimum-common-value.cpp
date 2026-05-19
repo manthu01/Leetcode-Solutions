5f1fd583fce6b5639abc9ceca145dec282096f91
Manthan <162015711+manthu01@users.noreply.github.com>
2026-05-20 00:58:38 +0530

    Time: 149 ms (5.03%) | Memory: 81.1 MB (5.19%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> n1;
+        map<int,int> n2;
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int limit = min(n, m);
+
+        int i = 0;
+
+        for(i = 0; i < limit; i++)
+        {
+            n1[nums1[i]]++;
+            n2[nums2[i]]++;
+
+            if(nums1[i] < nums2[i])
+            {
+                if(n1[nums1[i]] >= 1 && n2[nums1[i]] >= 1)
+                    return nums1[i];
+            }
+            else
+            {
+                if(n2[nums2[i]] >= 1 && n1[nums2[i]] >= 1)
+                    return nums2[i];
+            }
+        }
+
+        while(i < n)
+        {
+            n1[nums1[i]]++;
+
+            if(n2[nums1[i]] >= 1)
+                return nums1[i];
+
+            i++;
+        }
+
+        while(limit < m)
+        {
+            n2[nums2[limit]]++;
+
+            if(n1[nums2[limit]] >= 1)
+                return nums2[limit];
+
+            limit++;
+        }
+
+        return -1;
+    }
+};
